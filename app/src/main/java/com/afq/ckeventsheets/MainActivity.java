3bb4ca3aa5e8288ef9593b862d9445f65726f7b8
AfnanQDW <afnan.qdw@gmail.com>
2020-03-02 10:56:30 +0200

    Splash + Login

--- app/src/main/java/com/afq/ckeventsheets/MainActivity.java
+++ app/src/main/java/com/afq/ckeventsheets/MainActivity.java
@@ -2,7 +2,11 @@ package com.afq.ckeventsheets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        }, 3000);
+
     }
 }
